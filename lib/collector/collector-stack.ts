b39c1c0d912e3d1f466427b76c2c83f4cb401ab9
mprey <masonprey7@gmail.com>
2025-03-19 11:27:45 -0400

    feat(collector): bump version

--- lib/collector/collector-stack.ts
+++ lib/collector/collector-stack.ts
@@ -13,7 +13,7 @@ export interface CollectorStackProps extends cdk.StackProps {
 
 export class CollectorStack extends cdk.Stack {
 
-  private static ORCHESTRATOR_VERSION = "1.0.5";
+  private static ORCHESTRATOR_VERSION = "1.0.6";
 
   constructor(scope: Construct, id: string, props: CollectorStackProps) {
     super(scope, id, props);
