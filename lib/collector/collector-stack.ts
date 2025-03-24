f7943fa3307501a4004d10b2932d68610c99b97c
mprey <masonprey7@gmail.com>
2025-03-24 08:33:28 -0400

    misc(collector): bump orchestrator

--- lib/collector/collector-stack.ts
+++ lib/collector/collector-stack.ts
@@ -13,7 +13,7 @@ export interface CollectorStackProps extends cdk.StackProps {
 
 export class CollectorStack extends cdk.Stack {
 
-  private static ORCHESTRATOR_VERSION = "1.0.8";
+  private static ORCHESTRATOR_VERSION = "1.0.10";
 
   constructor(scope: Construct, id: string, props: CollectorStackProps) {
     super(scope, id, props);
