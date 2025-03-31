63395d0e1c4511ca33de4a3f4f67f8894a6a23dd
mprey <masonprey7@gmail.com>
2025-03-30 20:50:23 -0400

    fix(lamba): attempt #51235

--- lib/constructs/lambda.ts
+++ lib/constructs/lambda.ts
@@ -1,7 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as path from 'path';
 import * as fs from 'fs';
 import { Construct } from 'constructs';
@@ -23,7 +22,6 @@ export class OrchestratorLambda extends Construct {
     super(scope, id);
 
     const dockerDir = path.join(__dirname, `${props.lambdaName}-docker`);
-    const githubSecret = secretsmanager.Secret.fromSecretNameV2(this, 'GithubMaven', 'GITHUB_MAVEN');
 
     if (!fs.existsSync(dockerDir)) {
       fs.mkdirSync(dockerDir);
@@ -36,14 +34,9 @@ export class OrchestratorLambda extends Construct {
 
       RUN --mount=type=secret,id=MAVEN_CREDENTIALS \
         export MAVEN_CREDENTIALS=$(cat /run/secrets/MAVEN_CREDENTIALS) &&\
-        echo $MAVEN_CREDENTIALS
-      RUN echo $MAVEN_CREDENTIALS
-      RUN echo $MAVEN_CREDENTIALS | jq -r \'.GITHUB_ACTOR\'
-
-      RUN echo "Fetching Maven credentials..." && \
-          MAVEN_USERNAME=$(echo $MAVEN_CREDENTIALS | jq -r \'.GITHUB_ACTOR\') && \
-          MAVEN_PASSWORD=$(echo $MAVEN_CREDENTIALS | jq -r \'.GITHUB_TOKEN\') && \
-          wget --user=$MAVEN_USERNAME --password=$MAVEN_PASSWORD -O /var/task/lambda.jar "https://maven.pkg.github.com/gnome-trading-group/gnome-orchestrator/group/gnometrading/gnome-orchestrator/${props.orchestratorVersion}/gnome-orchestrator-${props.orchestratorVersion}.jar"
+        MAVEN_USERNAME=$(echo $MAVEN_CREDENTIALS | jq -r \'.GITHUB_ACTOR\') && \
+        MAVEN_PASSWORD=$(echo $MAVEN_CREDENTIALS | jq -r \'.GITHUB_TOKEN\') && \
+        wget --user=$MAVEN_USERNAME --password=$MAVEN_PASSWORD -O /var/task/lambda.jar "https://maven.pkg.github.com/gnome-trading-group/gnome-orchestrator/group/gnometrading/gnome-orchestrator/${props.orchestratorVersion}/gnome-orchestrator-${props.orchestratorVersion}.jar"
       
       CMD ["${props.classPath}::handleRequest"]
     `;
